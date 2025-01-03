b07fc4574be6dc4c29f6bfc93db77803cbea61b2
Xiao Hanyu <xiaohanyu1988@gmail.com>
2025-01-03 16:35:05 +0800

    feat: update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,84 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useConfig, useTheme } from 'nextra-theme-docs'
+
+const x2 = 8
+const x6 = 24
+
+const Logo = () => {
+  const { theme } = useTheme()
+
+  return (
+    <>
+      <Image
+        src="/static/logo/logo-64x64.png"
+        alt="Debug Health"
+        width={x6}
+        height={x6}
+        style={{
+          marginRight: x2,
+          filter: theme === 'dark' ? 'invert(1)' : 'none',
+        }}
+      />
+      <h1>Debug Health</h1>
+    </>
+  )
+}
+
 export default {
-  logo: <span>Crafting Health</span>,
+  // github
+  docsRepositoryBase: 'https://github.com/debughealth/debug.health/tree/main',
+
+  // page head
+  logo: <Logo />,
   project: {
-    link: "https://github.com/xiaohanyu/health.xiaohanyu.me",
+    link: 'https://github.com/debughealth/debug.health',
   },
+  chat: {
+    link: 'https://discord.gg/Dr3fsafA',
+  },
+
+  head() {
+    const { title, frontMatter } = useConfig()
+
+    const siteTitle = 'Debug Health'
+    const siteDescription = [
+      'A guide book for people who need to work long hours',
+      'behind a desk in a healthy way.',
+    ].join(' ')
+
+    const pageTitle = frontMatter.title || title
+    const pageDescription = frontMatter.description || siteDescription
+
+    const url = 'https://debug.health'
+
+    return (
+      <>
+        <title>{pageTitle || siteTitle}</title>
+        <meta property="og:title" content={pageTitle || siteTitle} />
+        <meta property="og:site_name" content={siteTitle} />
+        <meta property="og:description" content={pageDescription} />
+        <meta property="og:url" content={url} />
+        <meta
+          property="og:image"
+          content={`${url}/static/logo/logo-64x64.png`}
+        />
+        <meta property="og:image:width" content="64" />
+        <meta property="og:image:height" content="64" />
+      </>
+    )
+  },
+
+  // sidebar
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
+  },
+
+  // footer
+  footer: {
+    content: `© ${new Date().getFullYear()} Debug Health`,
+  },
+
   darkMode: true,
-};
+}
